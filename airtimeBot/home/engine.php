8ad2441682b0cb23a1e60d921789f9a7bb3478b5
Isaac <yeboah.determined.isaac@gmail.com>
2020-11-30 03:19:46 +0000

    update

--- airtimeBot/home/engine.php
+++ airtimeBot/home/engine.php
@@ -1,5 +1,13 @@
 <?php
 
+
+
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: access");
+// header("Access-Control-Allow-Methods: POST");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 function userlogin(){
         include 'db.php';
         extract($_POST);
@@ -12,28 +20,47 @@ function userlogin(){
       //  $result = mysqli_query($con, "SELECT * FROM airtimebot_users WHERE username = '$user' ");
        $result = mysqli_query($con, "SELECT * FROM airtimebot_users ");
 
-       if(!$result){
+        if(!$result){
          echo mysqli_error($con);
-       } else {
-           while($row = mysqli_fetch_array($result)) { 
+        } else {
+            while($row = mysqli_fetch_array($result)) { 
         // $pasword = password_hash($pasword, PASSWORD_DEFAULT);
         if ($row['username'] == $user ){
           // echo "this user is not present!"; echo $row['username'];
 
           
         if (password_verify($password, $row['PASSWORD'])) {
-          echo "success";
-      } else echo "wrong password";
+          // echo "success";
+
+            $response = array(
+              "status" => "success",
+              "message" => "login successful...!"
+            );
+
+              } else {
+                $response = array(
+                  "status" => "failed",
+                  "message" => "wrong password"
+                );
+              }
         }
 
             if ($row['username'] != $user ){
-              echo " username not found !!!";
+
+              $response = array(
+                "status" => "failed",
+                "message" => "username not found..!"
+              );
+
+
             }
 
+            $response = json_encode($response);
 
+            echo $response;
    
 
-       }
+        }
     }
 
 
